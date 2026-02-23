b597e762a6c3deffad67f8289b93b6a4afd37dd6
ptomer40 <ptomer40@yahoo.com>
2026-02-23 08:59:01 +0530

    node forntend

--- reactfrontend/src/App.jsx
+++ reactfrontend/src/App.jsx
@@ -5,17 +5,42 @@ import './App.css'
 
 function App() {
   const[data,setData]=useState([]);
+  const[loader,setLoader]=useState(false)
 async function getData(){
+
+  try{
+    setLoader(true)
        const response=await fetch('http://localhost:4007/data');
        const jsondata=await response.json();
        setData(jsondata.msg);
+  }catch(e){
+    console.log(e)
+  }
+  finally{
+     setLoader(false)
+  }
 
-  // alert("Hii....inside get");
+  
+}
+
+function cartData(dataitem){
+alert(dataitem.title)
 }
   return (
     <>
       <h2>Welocme to React App</h2>
-      {JSON.stringify(data)}
+      {
+          data.map((ele)=>(
+          <div style={{border:'2px solid red'}}>
+            <img src={ele.image} height={200} width={200}></img>
+            <h2>{ele.id}:{ele.title}</h2>
+            <h3>{ele.title}</h3>
+            <button onClick={()=>cartData(ele)}>Add to cart</button>
+          </div>
+        ))
+      }
+      {loader?(<h2 style={{color:'red'}}>Data is loading...</h2>):("")}
+      {/* {JSON.stringify(data)} */}
       <button onClick={getData}>FetchData</button>
     </>
   )
